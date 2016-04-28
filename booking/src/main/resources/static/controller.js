736f1cc67df0623a7e19bf6c73e7caa8730a5e9f
pawelkami <p.kaminski.6@stud.elka.pw.edu.pl>
2016-04-29 00:18:14 +0200

    Usunięto controller.js

--- booking/src/main/resources/static/controller.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', ['google.places']);
-
-
-myApp.controller("ctrlAutoComplete", ['$scope', function($scope){
-	$scope.search = function() {
-		var cityJson = angular.fromJson($scope.city);
-		console.log(cityJson.name);
-	};
-}]);
