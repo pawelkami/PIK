6cbe63825e885439274294e86f9dd6c6896b9bea
jmuczyns <jmuczyns@mion.elka.pw.edu.pl>
2016-04-30 00:25:36 +0200

    dodano filtr czasu pobytu wraz z kalendarzem cz2 (zapomnialem jednego pliku) :heart:

---
+++ booking/src/main/resources/static/calendar.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Jan on 29.04.2016.
+ */
+
+$(function () {
+    $('#datetimepicker6').datetimepicker();
+    $('#datetimepicker7').datetimepicker({
+        useCurrent: false //Important! See issue #1075
+    });
+    $("#datetimepicker6").on("dp.change", function (e) {
+        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker7").on("dp.change", function (e) {
+        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+    });
+});
